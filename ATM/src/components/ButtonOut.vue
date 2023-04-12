cc5439f1f2f4f693971fb68dbad5cfdd73cc6f41
jaserleney <yaser.keny@gmail.com>
2023-04-12 07:54:07 -0500

    changes

---
+++ ATM/src/components/ButtonOut.vue
@@ -0,0 +1,9 @@
+<template>
+  <button class="btn btn-pink"></button>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- ATM/src/views/CashOutView.vue
+++ ATM/src/views/CashOutView.vue
@@ -1,9 +1,26 @@
 <template>
-  <h1>CashOut</h1>
+  <div class="container">
+    <div class="container text-center py-3 mx-auto">
+      <h1 class="fw-bold">VivaBanco</h1>
+    </div>
+    <div class="container"></div>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style></style>
+<style scoped>
+* {
+  font-family: "Poppins", sans-serif !important;
+}
+.container {
+  width: 100%;
+  height: 100vh;
+  max-width: 1024px;
+  max-height: 768px;
+  color: #f2f2f2;
+  background-color: #131240;
+}
+</style>
