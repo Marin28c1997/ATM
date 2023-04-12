16be4ea06ac0f3ce72ddd80ebe243abd6bff30aa
jaserleney <yaser.keny@gmail.com>
2023-04-12 07:04:15 -0500

    save info

--- ATM/src/App.vue
+++ ATM/src/App.vue
@@ -1,26 +1,11 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
   <div>
-    <header>
-      <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-      <div class="wrapper">
-        <nav>
-          <RouterLink to="/">Home</RouterLink>
-          <RouterLink to="/about">About</RouterLink>
-        </nav>
-      </div>
-    </header>
     <RouterView />
   </div>
-  
-
-
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
